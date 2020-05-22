58b5a5d11fe85b9589e887a6555c4a3a2b6d9d07
Javier Molina <impjim@Impripost.local>
2020-05-22 16:21:28 -0300

    V 0.0.1

---
+++ SISDAL/BITACORA/Bitacora.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SISDAL.CRUD;
+using SISModels;
+
+namespace SISDAL.BITACORA
+{
+    public class Bitacora : ICrud<SISModels.BITACORA>
+    {
+        public void delete(SISModels.BITACORA entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(List<SISModels.BITACORA> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SISModels.BITACORA> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SISModels.BITACORA> getAll(SISModels.BITACORA entity)
+        {
+            using (SISCOMEntities db = new SISCOMEntities()) {
+                return db.BITACORA.ToList();
+            }
+        }
+
+        public SISModels.BITACORA getId(SISModels.BITACORA entity)
+        {
+            using (SISCOMEntities db = new SISCOMEntities()) {
+
+                return db.BITACORA.Find(entity.BITACORA_ID);
+            }
+        }
+
+        public void save(SISModels.BITACORA entity)
+        {
+            using (SISCOMEntities db = new SISCOMEntities())
+            {
+                db.BITACORA.Add(entity);
+                db.SaveChanges();
+            }
+        }
+
+        public void save(List<SISModels.BITACORA> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(SISModels.BITACORA entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(List<SISModels.BITACORA> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SISDAL/CRUD/ICrud.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISDAL.CRUD
+{
+    interface ICrud <T>
+    {
+
+        List<T> getAll();
+        List<T> getAll( T entity);
+        T getId(T entity);
+        void save(T entity);
+        void save(List<T> list);
+        void update(T entity);
+        void update(List<T> list);
+        void delete(T entity);
+        void delete(List<T> list);
+
+    }
+}
